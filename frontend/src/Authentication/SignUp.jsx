e9198687337b2ce89bd979e92cda3b14dfc35899
nitin <nimble2905@gmail.com>
2024-03-18 14:01:17 +0530

    some minor changes

--- frontend/src/Authentication/SignUp.jsx
+++ frontend/src/Authentication/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { FaEye, FaArrowRight } from "react-icons/fa";
 import { AuthContext } from "../Context/Authentication_context/AuthContext";
 
