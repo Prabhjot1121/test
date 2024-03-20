6775dd03e2378fbe44d4af1bc111a091e21a38b0
nitin <nimble2905@gmail.com>
2024-03-20 23:16:18 +0530

    some minor changes

--- backend/Index.js
+++ backend/Index.js
@@ -9,13 +9,7 @@ const app = express()
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 
-app.use(cors(
-    {
-        origin:["*"],
-        methods:["GET","POST","PUT","DELETE"],
-        credentials:true
-    }
-))
+app.use(cors())
 
 app.get("/", (req, res) => {
     return res.send("Hello World")
