7e226d17c90523212f5758886b3a842c257c4c94
nitin <nimble2905@gmail.com>
2024-04-06 09:29:50 +0530

    context to reduce prop drilling

---
+++ frontend/src/Context/Location_context/LocationContext.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { createContext, useState } from "react";
+
+export const LocationContext = createContext();
+
+export const LocationProvider = ({ children }) => {
+  const [location, setLocation] = useState(() => {
+    return localStorage.getItem("location") || "";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("location", location);
+  },[location]);
+
+  return (
+    <>
+      <LocationContext.Provider value={{ location, setLocation }}>
+        {children}
+      </LocationContext.Provider>
+    </>
+  );
+};
