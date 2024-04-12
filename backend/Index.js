c0540fd4f8c70d28396833886340dc1ea7e68c66
nitin <nimble2905@gmail.com>
2024-04-13 00:08:23 +0530

    added another api call for sending notification to user"s whatsapp number

--- backend/Index.js
+++ backend/Index.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const connectToMongo = require("./db/db")
-const cors = require('cors')
-const port = 8000
+const express = require("express");
+const connectToMongo = require("./db/db");
+const cors = require("cors");
+const port = 8000;
 
-connectToMongo()
-const app = express()
+connectToMongo();
+const app = express();
 
-app.use(express.json({ limit: "30mb", extended: true }))
-app.use(express.urlencoded({ limit: "30mb", extended: true }))
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
-    return res.send("Hello World")
-})
+  return res.send("Hello World");
+});
 
 // adding routes
-app.use('/api/auth', require('./routes/auth'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notify", require("./routes/notify"));
 
 app.listen(port, () => {
-    console.log(`utsav backend running on http://localhost:${port}`);
-})
+  console.log(`utsav backend running on http://localhost:${port}`);
+});
