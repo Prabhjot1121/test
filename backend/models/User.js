0f5ac3daf3f55c41557d8fc410c67405241e779c
nitin <nimble2405@gmail.com>
2024-04-14 11:20:53 +0530

    minor change

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,41 +1,39 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     profileImage: {
         data: String,
-        contentType: String
+        contentType: String,
     },
     name: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
-        required: true
+        required: true,
     },
     mobileNumber: {
         type: String,
         minLength: [10],
         maxLength: [10],
-        required: true
+        required: true,
     },
     address: {
         type: String,
-        required: true
+        required: true,
     },
     date: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+    },
+});
 
-    }
-
-})
-
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 module.exports = User;
