196110c466ea0db74646d862051dc093c3075e54
nitin <nimble2905@gmail.com>
2024-04-14 11:14:09 +0530

    created notification model

---
+++ backend/models/Notify.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const NotificationSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    contactNumber: {
+        type: String,
+        required: true,
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+    },
+    functionDate: {
+        type: String,
+        required: true
+    },
+    functionTime: {
+        type: String,
+        required: true,
+    },
+    functionType: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+const Notification = mongoose.model("Notification", NotificationSchema);
+module.exports = Notification;
