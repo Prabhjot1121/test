a10400f9e63d1036369f6c43fae5b1ab1bed4c01
nitin <nimble2405@gmail.com>
2024-04-16 23:44:43 +0530

    added another route to remove saved item

--- backend/routes/activity.js
+++ backend/routes/activity.js
@@ -3,71 +3,95 @@ const router = express.Router();
 const { validationResult } = require("express-validator");
 const SavedItem = require("../models/SavedItem");
 const fetchUser = require("../middleware/fetchUser");
-const User = require("../models/User")
+const User = require("../models/User");
 
 // saved vendor Item like venues or other subcategory particular data
 router.post("/saveItem", fetchUser, async (req, res) => {
-    const savedItemData = req.body;
-    const userId = req.user.id;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+  const savedItemData = req.body;
+  const userId = req.user.id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).json("Unauthorized access");
     }
 
-    try {
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(401).json("Unauthorized access");
-        }
-
-        const existingSavedItem = await SavedItem.findOne({ itemId: savedItemData.itemId, userId: userId });
-        if (existingSavedItem) {
-            return res.status(400).json("Item already saved by this user");
-        }
-
-        const savedItem = new SavedItem({
-            userId: userId,
-            userName: user.name,
-            itemId: savedItemData.itemId,
-            name: savedItemData.name,
-            image: savedItemData.image,
-            location: savedItemData.location,
-            foodCategory: savedItemData.foodCategory,
-            rating: savedItemData.rating,
-            itemSaved: true
-        });
-        const newSavedItem = await savedItem.save();
-        console.log(newSavedItem);
-        res.status(201).json(newSavedItem);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json("Internal server error");
+    const existingSavedItem = await SavedItem.findOne({
+      itemId: savedItemData.itemId,
+      userId: userId,
+    });
+    if (existingSavedItem) {
+      return res.status(400).json("Item already saved by this user");
     }
+
+    const savedItem = new SavedItem({
+      userId: userId,
+      userName: user.name,
+      itemId: savedItemData.itemId,
+      name: savedItemData.name,
+      image: savedItemData.image,
+      location: savedItemData.location,
+      foodCategory: savedItemData.foodCategory,
+      rating: savedItemData.rating,
+      itemSaved: true,
+    });
+    const newSavedItem = await savedItem.save();
+    console.log(newSavedItem);
+    res.status(201).json(newSavedItem);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Internal server error");
+  }
 });
 
+// remove savedItem from database if authorised
+router.delete("/removeItem/:id", fetchUser, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const userId = req.user.id;
+    const itemId = req.params.id;
+
+    const user = await User.findById(userId);
+    if (user) {
+      const savedItem = await SavedItem.findByIdAndDelete(itemId);
+      if (!savedItem) {
+        return res.status(400).json("Item not found");
+      }
+      res.status(200).json({ message: "Item removed successfully" });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Internal server error");
+  }
+});
 
 // fetch saved items with authentication
 router.get("/getSavedItems", fetchUser, async (req, res) => {
-    const userId = req.user.id;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const userId = req.user.id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    try {
-        const user = await User.findById(userId)
-        if (user) {
-            const allSavedItems = await SavedItem.find({ userId })
-            console.log(allSavedItems);
-            res.status(200).json(allSavedItems)
-        }
-        else {
-            return res.status(401).json("Unauthorised access")
-        }
-    } catch (error) {
-        res.status(500).json("Internal server error")
+  try {
+    const user = await User.findById(userId);
+    if (user) {
+      const allSavedItems = await SavedItem.find({ userId });
+      console.log(allSavedItems);
+      res.status(200).json(allSavedItems);
+    } else {
+      return res.status(401).json("Unauthorised access");
     }
-
+  } catch (error) {
+    res.status(500).json("Internal server error");
+  }
 });
 
 module.exports = router;
