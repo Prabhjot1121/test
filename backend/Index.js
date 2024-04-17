a8e4456ec3a3ceaafb9a4e5e606e7f448b2e2327
nitin <nimble2405@gmail.com>
2024-04-17 17:53:08 +0530

    applied cors

--- backend/Index.js
+++ backend/Index.js
@@ -9,7 +9,13 @@ const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://utsav.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   return res.send("Hello World");
