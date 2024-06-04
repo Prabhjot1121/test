2bb1d14dc2388ea762630ec0683ae6ea88ef0685
nitin <nimble2405@gmail.com>
2024-06-04 23:51:26 +0530

    removed unrequired data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import Home from "./pages/Home";
 import Navbar from "./Components/Navbar";
-import Features from "./pages/Features";
 import Vendors from "./pages/Vendors";
 import About from "./pages/About";
 import Contact from "./pages/Contacts/Contact";
@@ -19,12 +18,12 @@ import Dashboard from "./Components/Dashboard";
 import UserDetails from "./Components/UserDetails";
 import Settings from "./Components/Settings";
 import Activity from "./Components/Activity";
-import Premium from "./Components/Premium";
 import { AuthProvider } from "./Context/Authentication_context/AuthContext";
 import DisplayVendorsData from "./pages/DisplayVendorsData";
 import { LocationProvider } from "./Context/Location_context/LocationContext";
 import VendorDetailsPage from "./Components/VendorDetailsPage";
 import { ActivityProvider } from "./Context/Activity_context/ActivityContext";
+import Photos from "./pages/Photos";
 
 function App() {
   const { location, category, subCategory } = useParams();
@@ -54,7 +53,7 @@ function App() {
                   path="/vendors/:location/:category?/:all?/:subCategory?/:name?"
                   element={<VendorDetailsPage />}
                 />
-                <Route exact path="/features" element={<Features />} />
+                <Route exactt path="/photos" element={<Photos />} />
                 <Route exact path="/about" element={<About />} />
                 <Route exact path="/contact" element={<Contact />} />
                 <Route exact path="/logIn" element={<LogIn />} />
@@ -62,12 +61,7 @@ function App() {
                 <Route exact path="/userProfile/" element={<UserProfile />}>
                   <Route
                     exact
-                    path="/userProfile/dashboard"
-                    element={<Dashboard />}
-                  />
-                  <Route
-                    exact
-                    path="/userProfile/personal-Info"
+                    path="/userProfile/profile"
                     element={<UserDetails />}
                   />
                   <Route
@@ -82,11 +76,6 @@ function App() {
                     subCategory={subCategory}
                     location={location}
                     element={<Activity />}
-                  />
-                  <Route
-                    exact
-                    path="/userProfile/premium"
-                    element={<Premium />}
                   />
                 </Route>
               </Routes>
