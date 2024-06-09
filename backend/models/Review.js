bc2613a32cdbd3df68d2ec686004c564377848d9
nitin <nimble2405@gmail.com>
2024-06-09 08:18:13 +0530

    added another model for reviews

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -2,21 +2,28 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const User = require("./User");
 
-const reviewSchema = new Schema({
-  reviewText: {
-    type: String,
-    required: true,
+const reviewSchema = new Schema(
+  {
+    reviewText: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    itemId: {
+      type: Number,
+      required: true,
+    },
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: User,
-    required: true,
-  },
-  itemId: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+reviewSchema.index({ userId: 1, itemId: 1 }, { unique: true });
 
 const Reviews = mongoose.model("Reviews", reviewSchema);
 module.exports = Reviews;
