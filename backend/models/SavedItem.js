3406ceefed1d3f47983c9d8353076384cb25fdcb
nitin <nimble2405@gmail.com>
2024-06-09 08:19:05 +0530

    some minor changes

--- backend/models/SavedItem.js
+++ backend/models/SavedItem.js
@@ -2,64 +2,71 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const User = require("./User");
 
-const savedItem = new Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: User,
-  },
-  userName: {
-    type: String,
-    ref: User,
-  },
-  itemId: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    contentType: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  foodCategory: {
-    type: {
+const savedItem = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+    },
+    userName: {
       type: String,
-      enum: ["veg", "nonVeg"],
-      // required: true,
+      ref: User,
     },
-    vegPlatePrice: {
+    itemId: {
       type: Number,
-      required: function () {
-        return this.foodCategory === "veg";
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      contentType: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    foodCategory: {
+      type: {
+        type: String,
+        enum: ["veg", "nonVeg"],
+        // required: true,
+      },
+      vegPlatePrice: {
+        type: Number,
+        required: function () {
+          return this.foodCategory === "veg";
+        },
+      },
+      nonVegPlatePrice: {
+        type: Number,
+        required: function () {
+          return this.foodCategory === "nonVeg";
+        },
       },
     },
-    nonVegPlatePrice: {
-      type: Number,
-      required: function () {
-        return this.foodCategory === "nonVeg";
-      },
+    itemSaved: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
-  itemSaved: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+savedItem.index({ userId: 1, itemId: 1 }, { unique: true });
 
 const SavedItem = mongoose.model("SavedItem", savedItem);
 module.exports = SavedItem;
