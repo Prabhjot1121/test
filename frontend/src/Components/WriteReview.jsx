02eaec50ec057d738b4ae988c54fb5d69d37c1cb
nitin <nimble2405@gmail.com>
2024-06-09 13:32:46 +0530

    some basic changes in code format and fuctions

--- frontend/src/Components/WriteReview.jsx
+++ frontend/src/Components/WriteReview.jsx
@@ -1,41 +1,29 @@
-import React, { useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import toast from "react-hot-toast";
 import { FaPencilAlt } from "react-icons/fa";
+import { ActivityContext } from "../Context/Activity_context/ActivityContext";
 
-const WriteReview = ({ venue }) => {
-  const [reviewText, SetReviewText] = useState("");
-  const inputRef = useRef(null);
-  const token = localStorage.getItem("token");
-  const host = "http://localhost:8000/api/activity/saveReview";
+const WriteReview = () => {
+  const {
+    venue,
+    saveReview,
+    token,
+    setReviewText,
+    reviewText,
+    inputRef,
+    handleClear,
+  } = useContext(ActivityContext);
 
-  const handleClear = (e) => {
-    inputRef.current.value = "";
-  };
   const onChange = (e) => {
-    SetReviewText(e.target.value);
+    setReviewText(e.target.value);
   };
+
   const handleSaveReview = async (e) => {
     e.preventDefault();
     if (token) {
-      const response = await fetch(`${host}`, {
-        method: "POST",
-        headers: {
-          "auth-token": token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ reviewText, itemId: venue.itemId }),
-      });
-      if (!response.ok) {
-        console.log(response);
-        toast.error("could not post review");
-        throw new Error("Failed to process this request");
-      }
-      const data = response.json;
-      console.log(data);
-      toast.success("Review Submitted!");
-      handleClear();
-    }else{
-      toast("Please login first")
+      saveReview();
+    } else {
+      toast("Please login first");
     }
   };
   return (
@@ -51,22 +39,23 @@ const WriteReview = ({ venue }) => {
           <textarea
             ref={inputRef}
             name="reviewContent"
-            //   value={reviewText}
             onChange={onChange}
             className="w-full rounded-sm p-2 focus:outline-red-200"
             placeholder="Type here"
+            value={reviewText}
           ></textarea>
           {/* buttons */}
           <div className="flex space-x-2 w-full justify-end">
             <button
+              type="button"
               onClick={handleClear}
-              className="font-medium shadow-sm shadow-red-900 px-4 py-2 bg-red-600 hover:bg-red-700 duration-300 text-white rounded-md"
+              className="font-medium shadow-sm  shadow-red-900 px-4 py-2 bg-red-600 hover:bg-red-700 duration-300 text-white rounded-md"
             >
               Clear
             </button>
             <button
               type="submit"
-              className="font-medium shadow-sm  shadow-red-900 px-4 py-2 bg-red-600 text-white rounded-md"
+              className="font-medium shadow-sm  shadow-red-900 px-4 py-2 bg-red-600 hover:bg-red-700 duration-300 text-white rounded-md"
             >
               Submit
             </button>
